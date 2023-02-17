c1c8b2d27bdd6c71128cd4be7804ab1dac380048
Kendall Bramlett <kmbramlett@crimson.ua.edu>
2023-02-16 19:48:18 -0600

    final

--- assignment2KendallBramlett/assignment2KendallBramlett/wwwroot/js/script.js
+++ assignment2KendallBramlett/assignment2KendallBramlett/wwwroot/js/script.js
@@ -7,12 +7,13 @@ function apiSearch() {
     "count": "50",
     "offset": "0",
     "mkt": "en-us"
-  };
+    };
+ 
 
   $.ajax({
-      url: 'my-api-url' + $.param(params),
+      url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
       beforeSend: function (xhrObj) {
-        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "my-api-key");
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "153ff0c0683f46219502331ffebac10f");
       },
       type: "GET",
     })
@@ -20,7 +21,9 @@ function apiSearch() {
       len = data.webPages.value.length;
       for (i = 0; i < len; i++) {
         results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
-      }
+        }
+        var Search = document.getElementById("searchResults");
+        Search.style.visibility = "visible";
 
       $('#searchResults').html(results);
       $('#searchResults').dialog();
